df889e59b7899f88440f386c2ad5509f8dec0afd
J-Rozas <jrozaslima@gmail.com>
2021-01-03 14:37:22 +0000

    Styled the file.

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,9 @@
-dictionary = File.read("../dictionary.txt").split(" ")
-mistery_word = dictionary.sample()
-puts mistery_word
+# frozen_string_literal: true
 
+dictionary = File.read('../dictionary.txt').split(' ')
+mistery_word = dictionary.sample
 
+# class to create a new game
 class Game
   attr_accessor :word
 
@@ -11,40 +12,36 @@ class Game
   end
 
   def play_game
-    word_to_guess = self.word
+    word_to_guess = word
     word_len = word_to_guess.length
-    word_holder = Array.new
+    word_holder = []
     num = 0
     while num < word_len
-      if (word_to_guess[num] == "-")
-        word_holder.push("-")
+      if word_to_guess[num] == '-'
+        word_holder.push('-')
       else
-        word_holder.push("_")
+        word_holder.push('_')
       end
       num += 1
     end
     hanged = 0
     p word_holder
     while hanged != 6
-      print "Type a letter: "
+      print 'Type a letter: '
       user_letter = gets.chomp!.downcase
       if word_to_guess.downcase.include?(user_letter)
-        word_holder.each_with_index do |element, index|
-          if word_to_guess[index] == user_letter
-            word_holder[index] = user_letter
-          end
+        word_holder.each_with_index do |_element, index|
+          word_holder[index] = user_letter if word_to_guess[index] == user_letter
         end
-        if word_holder.join("") == word_to_guess
+        if word_holder.join('').== word_to_guess.downcase
           puts "You go it! The word was, indeed, \"#{word_to_guess}\"."
           break
         end
       else
-        hanged += 1  
+        hanged += 1
       end
       p word_holder
-      if hanged == 6
-        puts "You've lost, the correct word was \"#{word_to_guess}\"."
-      end
+      puts "You've lost, the correct word was \"#{word_to_guess}\"." if hanged == 6
     end
   end
 end
