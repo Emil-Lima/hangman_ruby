5fda4cafc4187e3469839aca4f361a51bfe63e44
J-Rozas <jrozaslima@gmail.com>
2021-01-04 17:26:56 +0000

    Styled the code.

--- lib/game.rb
+++ lib/game.rb
@@ -1,13 +1,12 @@
 # frozen_string_literal: true
 
-require "yaml"
+require 'yaml'
 
 # Module containing all functions to run the game
 module AllFunctions
   def gettable_word
     dictionary = File.read('../dictionary.txt').split(' ')
-    mistery_word = dictionary.sample
-    mistery_word
+    dictionary.sample
   end
 
   def word_holder(word)
@@ -15,79 +14,77 @@ module AllFunctions
     num = 0
     while num < word_len
       if word[num] == '-'
-        self.word_array.push('-')
+        word_array.push('-')
       else
-        self.word_array.push('_')
+        word_array.push('_')
       end
       num += 1
     end
   end
 
   def round
-    letter = user_letter()
-    if self.original_word.include?(letter)
-      self.right_letters.push(letter)
-      self.word_array.each_with_index do |_element, index|
-        self.word_array[index] = letter if self.original_word[index] == letter
+    letter = user_letter
+    if original_word.include?(letter)
+      right_letters.push(letter)
+      word_array.each_with_index do |_element, index|
+        word_array[index] = letter if original_word[index] == letter
       end
     else
-      self.wrong_letters.push(letter)
+      wrong_letters.push(letter)
       self.num_rounds += 1
     end
   end
 
   def user_letter
-    print "Type a letter: "
-    letter = gets.chomp!.downcase
-    letter
+    print 'Type a letter: '
+    gets.chomp!.downcase
   end
 
   def save_or_continue
-    print "Do you want to save (s) the current game or continue (c)?: "
-    reply = gets.chomp!
-    reply
+    print 'Do you want to save (s) the current game or continue (c)?: '
+    gets.chomp!
   end
 
   def save_game
     Dir.mkdir 'output' unless Dir.exist? 'output'
-    @filename = "1_game.yaml"
+    @filename = '1_game.yaml'
     File.open("output/#{@filename}", 'w') { |file| file.write save_to_yaml }
   end
 
   def save_to_yaml
     YAML.dump(
-      'original_word' => self.original_word,
-      'available_letters' => self.available_letters, #Maybe delete this
-      'right_letters' => self.right_letters,
-      'wrong_letters' => self.wrong_letters,
-      'word_array' => self.word_array,
+      'original_word' => original_word,
+      'available_letters' => available_letters, # Maybe delete this
+      'right_letters' => right_letters,
+      'wrong_letters' => wrong_letters,
+      'word_array' => word_array,
       'num_rounds' => self.num_rounds
     )
   end
 
   def load_game
-    file = YAML.safe_load(File.read("output/1_game.yaml"))
+    file = YAML.safe_load(File.read('output/1_game.yaml'))
     self.original_word = file['original_word']
-    self.right_letters = file["right_letters"]
-    self.wrong_letters = file["wrong_letters"]
+    self.right_letters = file['right_letters']
+    self.wrong_letters = file['wrong_letters']
     self.word_array = file['word_array']
     self.num_rounds = file['num_rounds']
-    puts self.original_word
-    p self.word_array
+    puts original_word
+    p word_array
     while self.num_rounds < 6
-      if save_or_continue() == "c"
-        round()
-        p self.word_array 
-        if self.original_word == self.word_array.join("")
-          puts "You win!"
+      if save_or_continue == 'c'
+        round
+        p word_array
+        if original_word == word_array.join('')
+          puts 'You win!'
           break
         end
       else
-        save_game()
+        save_game
         break
       end
       if self.num_rounds == 6
-        puts "You have lost. The right word was #{self.original_word}."
+        puts "You have lost. The right word was #{original_word}."
         break
       end
     end
@@ -101,40 +98,40 @@ class Game
   attr_accessor :original_word, :wrong_letters, :right_letters, :available_letters, :num_rounds, :word_array
 
   def initialize
-    self.available_letters = ("a".."z").to_a
+    self.available_letters = ('a'..'z').to_a
     self.right_letters = []
     self.wrong_letters = []
     self.word_array = []
     self.num_rounds = 0
-    play_game()
+    play_game
   end
 
   def play_game
-    print "Play a new game (n) or load an existing game file (l)."
+    print 'Play a new game (n) or load an existing game file (l).'
     user_says = gets.chomp!
-    new_game() if user_says == "n"
-    load_game() if user_says == "l"
+    new_game if user_says == 'n'
+    load_game if user_says == 'l'
   end
 
   def new_game
-    self.original_word = gettable_word()
-    word_holder(self.original_word)
-    puts self.original_word
-    p self.word_array
+    self.original_word = gettable_word
+    word_holder(original_word)
+    puts original_word
+    p word_array
     while self.num_rounds < 6
-      if save_or_continue() == "c"
-        round()
-        p self.word_array 
-        if self.original_word == self.word_array.join("")
-          puts "You win!"
+      if save_or_continue == 'c'
+        round
+        p word_array
+        if original_word == word_array.join('')
+          puts 'You win!'
           break
         end
       else
-        save_game()
+        save_game
         break
       end
       if self.num_rounds == 6
-        puts "You have lost. The right word was #{self.original_word}."
+        puts "You have lost. The right word was #{original_word}."
         break
       end
     end
